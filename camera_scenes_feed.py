97dd11bdc5e8a184436546ebc9c58caaeb91929c
mhashim6 <m6hashim@gmail.com>
2020-02-27 14:33:25 +0200

    start with a first scene.

--- camera_scenes_feed.py
+++ camera_scenes_feed.py
@@ -4,11 +4,13 @@ from scenedetect.detectors import ContentDetector
 from typing import Callable
 
 
-def run_video_cam(new_scene_callback: Callable):
+def run_video_cam(on_new_scene: Callable):
     cam = cv2.VideoCapture(0)
+    _, first_scene = cam.read()
+    on_new_scene(first_scene)
     scene_manager = SceneManager()
     scene_manager.add_detector(ContentDetector())
-    scene_manager.detect_scenes(frame_source=cam, callback=new_scene_callback)
+    scene_manager.detect_scenes(frame_source=cam, callback=on_new_scene)
 
 
 if __name__ == '__main__':

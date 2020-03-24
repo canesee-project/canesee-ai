fd1f63033fb34de487bf38f18f590f43b58211b4
Adel Abu Hashim <adel.muha16@gmail.com>
2020-03-24 12:48:57 +0200

    seperate data in file

--- facial_recognition/README.md
+++ facial_recognition/README.md
@@ -1,10 +1,14 @@
 # Facial Recognition
 This model can recoginze people who are in dataset
 
+
 ## Software Needs
+
 
 ## Built with
 
+
 ## Files used
+
 
 ## Original Repo
--- facial_recognition/face_reco.py
+++ facial_recognition/face_reco.py
@@ -3,13 +3,18 @@
 """
 
 import face_recognition as face
+import data
 
-known_faces = {}
+
+known_faces = data.known_faces
 
 def init ():
-    """Load dataset"""
+    '''
+    add test data
+    '''
 
     global known_faces
+
     image_of_adel = face.load_image_file("adel.jpg")
     image_of_luis = face.load_image_file("luis.jpg")
     image_of_josh = face.load_image_file("josh.jpg")
@@ -18,9 +23,15 @@ def init ():
     face_encoding_of_luis = face.face_encodings(image_of_luis)[0]
     face_encoding_of_josh = face.face_encodings(image_of_josh)[0]
 
-    known_faces = {"adel": face_encoding_of_adel,
-                   "luis": face_encoding_of_luis,
-                   "josh": face_encoding_of_josh}
+    known_faces = {"adel": face_encoding_of_adel.tolist(),
+                   "luis": face_encoding_of_luis.tolist(),
+                   "josh": face_encoding_of_josh.tolist()}
+
+
+    f = open("data.py", "w")
+    f.write("known_faces = {}".format(known_faces))
+    f.close()
+
 
 
 def new_face (image, name):
@@ -28,8 +39,11 @@ def new_face (image, name):
     updates dict"""
     global known_faces
 
-    #image = face.load_image_file(image)
-    known_faces[name] = face.face_encodings(image)
+    image = face.load_image_file(image)
+    known_faces[name] = face.face_encodings(image)[0].tolist()
+    f = open("data.py", "w")
+    f.write("known_faces = {}".format(known_faces))
+    f.close()
 
 
 
@@ -37,7 +51,7 @@ def new_face (image, name):
 def reco(image):
     """Return a string of the name of person in the photo. """
     global known_faces
-    ##input_image = face.load_image_file(image)
+    #input_image = face.load_image_file(image)
     input_image = image
     input_image_encodings = face.face_encodings(input_image)  # convert photo into array of numbers
     known_faces_encodings = list(known_faces.values())
@@ -50,9 +64,11 @@ def reco(image):
 
 
 
-init()
-new_face("luis3.png","luis")
+#init() #for first time
+#new_face("luis2.png",'new')
 
-print (known_faces)
+
+
+
 
 

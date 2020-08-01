629a7120e591386deb2e34ad3a1378437a59b8d2
Muhammad Hashim <m6hashim@gmail.com>
2020-08-01 16:57:46 +0200

    update obj_reco. add face_reco

--- initializer.py
+++ initializer.py
@@ -1,11 +1,14 @@
 # @author mhashim6 on 2/26/20
 
 
-from object_detection.objectDetection_lite import init as init_object_detection
+from object_detection.tflite_objectDetection import init as init_object_detection
+from facial_recognition.face_reco import init as init_face_reco
 from utils import log
 
 
 def init_models():
     log('init object detection...')
     init_object_detection()
+    log('init facial recognition...')
+    init_face_reco()
     # TODO: other models

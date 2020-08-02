a88a4bea7479afb42115d3fc91e83f482e75c2a0
Mostafa Azazi <mostafa.3zazi@gmail.com>
2020-08-02 20:41:03 +0200

    Update image_captioning_interface.py

--- scenes/image_captioning_interface.py
+++ scenes/image_captioning_interface.py
@@ -6,9 +6,9 @@ from PIL import Image
 import os
 import cv2
 
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+# abspath = os.path.abspath(__file__)
+# dname = os.path.dirname(abspath)
+# os.chdir(dname)
 
 #max_length of_train_sequences
 max_length=46
@@ -31,10 +31,9 @@ class image_captioning ():
         infile.close()
 
     def preprocess_image_nparray(self,image):
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        img = tf.convert_to_tensor(img, dtype=tf.uint8)
+        img = tf.convert_to_tensor(image, dtype=tf.uint8)
         #try for DCT
-        # img = tf.convert_to_tensor(img, dtype=tf.float32)
+        # img = tf.convert_to_tensor(image, dtype=tf.float32)
         # s0, s1, s2 = tf.split(img, num_or_size_splits=3, axis=2)
         # s0 = tf.signal.dct(s0,type=3)
         # s1 = tf.signal.dct(s1,type=3)
@@ -52,15 +51,42 @@ class image_captioning ():
 
     def load_image(self,image_path):
         img = tf.io.read_file(image_path)
+        # img = tf.image.decode_jpeg(img, dct_method = "INTEGER_ACCURATE",channels=3)
+        img = tf.image.decode_jpeg(img,channels=3)
+        img = tf.image.resize(img, (224, 224))
+        img = tf.keras.applications.mobilenet_v2.preprocess_input(img) 
+        return img
+
+    def encode(self,image):
+        img = tf.convert_to_tensor(image, dtype=tf.uint8)
+        img = tf.image.encode_jpeg(img,
+            quality=100,
+            progressive=False,
+            optimize_size=False,
+            chroma_downsampling=True,
+            density_unit='in',
+            x_density=300,
+            y_density=300)
+        # img = tf.image.encode_png(
+        #     img, compression=-1, name=None)
         img = tf.image.decode_jpeg(img, channels=3)
         img = tf.image.resize(img, (224, 224))
         img = tf.keras.applications.mobilenet_v2.preprocess_input(img) 
         return img
 
-    def generate_from_saving_img(self,image):
+    def generate_from_img_nparray_encode(self,image):
+        im = tf.expand_dims(self.encode(image), 0)
+        return self.generate_caption(im)
+
+    def generate_from_saving_img_opencv(self,image):
         image_path = "temp.jpg"
         cv2.imwrite(image_path,image,[cv2.IMWRITE_JPEG_QUALITY,100,cv2.IMWRITE_JPEG_PROGRESSIVE,1,cv2.IMWRITE_JPEG_OPTIMIZE,1\
         ,cv2.IMWRITE_JPEG_LUMA_QUALITY,100,cv2.IMWRITE_JPEG_CHROMA_QUALITY,100])
+        return self.generate_from_img_path(image_path)
+    def generate_from_saving_img_pillow(self,image):
+        image_path = "temp.jpg"
+        image = Image.fromarray(image)
+        image.save(image_path)
         return self.generate_from_img_path(image_path)
 
     def generate_from_img_nparray(self,image):
@@ -101,8 +127,19 @@ if __name__ == '__main__':
     model = image_captioning (mobile_net_v2_weights='mobilenet_v2_weights_1.4.h5')
     while(1):
         test_image = input("enter path:")
-        im = cv2.imread(test_image)
-        # print(model.generate_from_img_nparray(im))
-        print(model.generate_from_saving_img(im))
-        print(model.generate_from_img_path(test_image))
+
+        #opencv
+        # im = cv2.imread(test_image)
+        # im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+
+        #pillow
+        im = Image.open(test_image)
+        im = np.array(im)
+        
+        print("generate_from_img_nparray_encode\n" + model.generate_from_img_nparray_encode(im))
+        print("generate_from_img_nparray\n" +model.generate_from_img_nparray(im))
+        print("generate_from_saving_img_opencv\n" +model.generate_from_saving_img_opencv(im))
+        print("generate_from_saving_img_pillow\n" +model.generate_from_saving_img_pillow(im))
+        print("generate_from_img_path\n" +model.generate_from_img_path(test_image)) #take path
         print('\n')
+        
